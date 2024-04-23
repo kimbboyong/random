2b8289a41ed60b8939ec1915f7bba2c498281b82
kimbboyong <kimbboyong@gmail.com>
2024-04-23 15:53:02 +0900

    login

--- src/pages/Result/index.jsx
+++ src/pages/Result/index.jsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { db } from "../../firebaseConfig";
 import { ref, onValue, set, push } from "firebase/database";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const Result = () => {
   const [result, setResult] = useState("");
   const [results, setResults] = useState([]);
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+  if (user) {
+    console.log("사용자가 로그인되었습니다.", user);
+  } else {
+    console.log("사용자가 로그인되지 않았습니다.");
+  }
 
   useEffect(() => {
     const resultsRef = ref(db, "yutnoriResults");
@@ -37,6 +46,20 @@ const Result = () => {
     });
   };
 
+  function loginUser(email, password) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // 로그인 성공
+        const user = userCredential.user;
+        console.log("로그인 성공:", user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("로그인 실패:", errorCode, errorMessage);
+      });
+  }
+  loginUser("test@test.com", "12341234");
   return (
     <div>
       <button onClick={handleClick}>윷 던지기</button>
